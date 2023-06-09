c20a908ff90ca205ca21d0b789c2d73ea79ad29d
sergoromanov <33855444+sergoromanov@users.noreply.github.com>
2023-06-10 07:59:47 +0900

    Add files via upload

---
+++ main.php
@@ -0,0 +1,155 @@
+<?php 
+session_start();
+$id=$_GET['id'];
+$con=mysqli_connect('localhost','root','root','social');
+$query=mysqli_query($con, "SELECT * FROM users WHERE id='$id'");
+$result=$query->fetch_assoc();
+$_SESSION['user'] = [
+    'id' => $result['id'],
+    'full_name' => $result['full_name'],
+    'email' => $result['email'],
+    'avatar' => $result['avatar']
+];
+    $userId = $_GET['id'];
+    $_SESSION['userId'] = $userId;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chatty</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <style type="text/css">
+   .log{
+	color: red;
+	}
+	.head{
+		color: black;
+		font-weight: bolds;
+	}
+
+    </style>
+</head>
+<body class="bg-light ">
+	<div class="container-fluid bg-white">
+		
+	</div>
+	<div class="container">
+		<div class="row">
+			<!--левая колонка-->
+			<div class="col-3">
+				<div class="bg-white mt-2 rounded">
+					<div>
+						<img src="<?= $_SESSION['user']['avatar'] ?>" class="w-100">
+					</div>
+
+					<div class="row">
+						<div class="col-8">
+							<div>
+								<a href="#" class="head"><?= $_SESSION['user']['full_name'] ?></a>
+							</div>
+							<div>
+								<a href="#"><?= $_SESSION['user']['email'] ?></a>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+					</div>
+
+				</div>
+				<!--topics-->
+				<div class="bg-white rounded ">
+					<a href="vendor/logout.php" class="log">Выход</a>
+					<a href="update.php?id=<?php echo $userId; ?>">Редактирование профиля</a>
+				</div>
+			</div>
+			<!---middle колонка--->
+			<div class="col-6">
+				<div class="row">
+					<div class="col-2">
+						<img src="<?= $_SESSION['user']['avatar'] ?>" class="rounded-circle w-100">
+					</div>
+					<div class="col-10">
+							 <form action="tweet.php" enctype="multipart/form-data" method="POST">
+							 	<input type="hidden" name="nick" value="<?php echo $result['login']?>">
+							 	<input type="hidden" name="id" value="<?php echo $result['id']?>">
+							 	<input name="img" type="file">
+							 	<input type="" name="text">
+							 	<button >Добавить</button>
+							 </form>
+					</div>
+				</div>
+				<div class="row">
+					<!--post1 begins-->
+			        <?php 
+			        $query = mysqli_query($con, 'SELECT * FROM post ORDER BY id DESC');
+					?>
+					<?php for ($i = 0; $i < $query->num_rows; $i++) { ?>
+						<?php  $post=$query->fetch_assoc(); ?>
+			            <div class="col-2">
+			              <img src="<?= $_SESSION['user']['avatar'] ?>" class="rounded-circle w-50">
+			            </div>
+			            <div class="col-10">
+			              	<div class="row">
+			                	<h5>
+			                 	 	<a href="#" class="text-dark"><?php echo $post['name'] ?></a>
+			                	</h5>
+			              	</div>
+			              	<div>
+			                	<p><?php echo $post['text'] ?></p>
+			              	</div>
+			              	<div>
+				                <img src="<?php  echo $post['img']?>" class="w-100 rounded">
+				                <form action="delete.php" method="POST">
+				                	<input type="hidden" name="<?php echo $result['id']?>">
+				                	<a href="delete.php?post_id=<?= $post['id'] ?>">Удалить</a>
+				                	</form>
+				                	<form action="vendor/bca.php" method="POST">
+				                		<input type="hidden" name="<?php echo $result['id']?>">
+				                		<button name="change" value="<?php echo $post['id'] ?>">Редактировать</button>
+				                	</form>
+			              	</div>
+			              	<div class="row">
+				                <div class="col-3">
+				                  <img src="images/like.png">
+				                </div>
+				                <div class="col-3">
+				                  <img src="images/comment.png">
+				                </div>
+				                <div class="col-3">
+				                   <img src="images/envelope.png">
+				                </div>
+				                <div class="col-3">
+				                   <img src="images/retweet.png">
+				                </div>
+			              	</div>
+			            </div>
+			           <?php } ?>
+					<!--post 1 ends-->
+				</div>
+			</div>
+			<!---right колонка--->
+			<div class="col-3">
+				<div class="bg-white rounded">
+					<div class="row ml-2">
+						<h2>Chatty</h2>
+					</div>
+					<div class="row">
+						<div class="col-4">
+							<img src="images/1.jpeg" class="rounded-circle w-100 ">
+						</div>
+						<div class="col-8">
+							<h4>Welcome to Chatty</h4>
+							<p class="text-secondary">@chatty</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 bg-white mt-1 rounded">
+					The best social network...ever
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ social.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 10 2023 г., 01:56
+-- Версия сервера: 8.0.19
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `social`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `post`
+--
+
+CREATE TABLE `post` (
+  `id` int NOT NULL,
+  `name` varchar(355) NOT NULL,
+  `text` varchar(355) NOT NULL,
+  `img` varchar(355) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `post`
+--
+
+INSERT INTO `post` (`id`, `name`, `text`, `img`) VALUES
+(51, 'killler', 'авыаыаыва', 'img/dlinnye-foto-4.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `full_name` varchar(355) DEFAULT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(500) DEFAULT NULL,
+  `avatar` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES
+(1, 'Романов Сергей Максимович', 'rmvsrg2', 'minelaunter@gmail.com', '1234567', 'uploads/1686347057computer-icons-icon-design-avatar-png-favpng-X29r5WhWMXVYvNsYXkR4iBgwf.jpg'),
+(2, 'Михаил Иванович Шишкин', 'misha', 'gangyyy@mail.ru', '1234', 'uploads/1686350007ривг.jpg'),
+(8, 'Романов Сергей Максимович', 'killler', 'sergey@gmail.com', '123', 'img/1686340158');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
